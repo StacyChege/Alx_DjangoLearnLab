197ac4a78db999ef8f406bc2b702b8c34418a23e
Stacy Chege <stacychege23@gmail.com>
2025-07-20 23:24:16 +0300

    Update template path in LibraryDetailView for correct rendering of library details

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,7 @@
+# relationship_app/views.py
 from django.shortcuts import render
-from django.views.generic import DetailView 
-from .models import Book, Library
-
-
-# Create your views here.
+from django.views.generic import DetailView # For class-based view
+from .models import Book, Library # <--- ENSURE THIS LINE IS EXACTLY AS SHOWN (importing Library)
 
 # Function-based view to list all books
 def book_list_view(request):
@@ -12,12 +10,10 @@ def book_list_view(request):
         'books': books
     }
     # Render the 'relationship_app/list_books.html' template, passing the books data
-    return render(request, 'relationship_app/list_books.html', context) # <--- Change the template path here
+    return render(request, 'relationship_app/list_books.html', context)
 
 # Class-based view to display details for a specific library
 class LibraryDetailView(DetailView):
     model = Library # Specify the model this view will work with
-    template_name = 'library_detail.html' # Specify the template to render
+    template_name = 'relationship_app/library_detail.html' # <--- CHANGE THIS LINE
     context_object_name = 'library' # The variable name to use in the template (e.g., {{ library.name }})
-    # For DetailView, Django automatically looks for an object using the 'pk' (primary key)
-    # from the URL pattern.
