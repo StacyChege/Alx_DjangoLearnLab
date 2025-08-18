0fa9dafeae9325d4a2a5f3379050da27d483d270
Stacy Chege <stacychege23@gmail.com>
2025-08-18 12:28:15 +0300

    Refactor CSS and HTML structure: update styles, enhance navigation, and set template directories

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -12,6 +12,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logged_out.html'), name='logout'),
 
-    # You'll need a home view to show your blog posts later, for now, a placeholder is fine.
-    path('', views.register, name='home'),
+    path('', views.home, name='home'),
 ]
