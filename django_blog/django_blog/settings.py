fde290f9fcbb508a07643ddbe7e53e00c16a38fc
Stacy Chege <stacychege23@gmail.com>
2025-08-18 12:54:09 +0300

    Fix static file paths in login and register templates; update settings to remove template directories

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,9 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
