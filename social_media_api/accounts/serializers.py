64c473a3661a739928379fc8c299c19b2b2dea6e
Stacy Chege <stacychege23@gmail.com>
2025-08-23 22:06:31 +0300

    Implement initial setup for accounts app: add models, serializers, views, and URLs; configure settings and migrations

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,23 @@
+
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'password2', 'bio', 'profile_picture']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
